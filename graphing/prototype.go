d9b7be675224e4d2e3ae275a755fcbdfcae55625
Lennart Oldenburg <hello@lennartoldenburg.de>
2018-05-22 20:12:23 -0700

    Added code for creating intersection prototype if precondition of run is achieved eventually.

--- graphing/prototype.go
+++ graphing/prototype.go
@@ -2,6 +2,7 @@ package graphing
 
 import (
 	"fmt"
+	"strings"
 )
 
 // CreatePrototype
@@ -11,7 +12,7 @@ func (n *Neo4J) CreatePrototype(iters []uint) error {
 
 	stmtCondGoals, err := n.Conn1.PrepareNeo(`
         MATCH (g1:Goal {run: {run}, condition: {condition}})
-        OPTIONAL MATCH (g2:Goal {run: {run}, condition: {condition}, condition_holds: true})
+        OPTIONAL MATCH (g2:Goal {run: {run}, condition: "pre", condition_holds: true})
         WITH g1, collect(g2) AS existsSuccess
         WHERE size(existsSuccess) > 0
         RETURN collect(g1.label) AS goals;
@@ -19,6 +20,12 @@ func (n *Neo4J) CreatePrototype(iters []uint) error {
 	if err != nil {
 		return err
 	}
+
+	var protoProv string
+
+	achvdCond := 0
+	allProv := make(map[string]int)
+	iterProv := make([]map[string]bool, len(iters))
 
 	for i := range iters {
 
@@ -42,13 +49,66 @@ func (n *Neo4J) CreatePrototype(iters []uint) error {
 			return err
 		}
 
-		fmt.Printf("\n%d:\n'%#v'\n", i, condGoalsAll)
+		iterProv[i] = make(map[string]bool)
+
+		for j := range condGoalsAll {
+
+			for k := range condGoalsAll[j] {
+
+				labels := condGoalsAll[j][k].([]interface{})
+
+				if len(labels) > 0 {
+					achvdCond += 1
+				}
+
+				for l := range labels {
+
+					label := labels[l].(string)
+
+					allProv[label] += 1
+					iterProv[i][label] = true
+				}
+			}
+		}
 	}
+
+	for label := range allProv {
+
+		if allProv[label] == achvdCond {
+
+			// Label is present in all label sets.
+			// Add it to final (intersection) prototype.
+			if protoProv == "" {
+				protoProv = fmt.Sprintf("['%s'", label)
+			} else {
+				protoProv = fmt.Sprintf("%s, '%s'", protoProv, label)
+			}
+		}
+	}
+
+	// Finish list.
+	protoProv = fmt.Sprintf("%s]", protoProv)
 
 	err = stmtCondGoals.Close()
 	if err != nil {
 		return err
 	}
+
+	exportQuery := `CALL apoc.export.cypher.query("
+	MATCH path = (r:Goal {run: 0, condition: 'post'})-[*0..]->(l:Goal {run: 0, condition: 'post'})
+	WHERE r.label IN ###PROTOTYPE### AND l.label IN ###PROTOTYPE###
+	RETURN path;
+	", "/tmp/export-prototype-post", {format: "cypher-shell", cypherFormat: "create"})
+	YIELD file, source, format, nodes, relationships, properties, time
+	RETURN file, source, format, nodes, relationships, properties, time;`
+
+	tmpExportQuery := strings.Replace(exportQuery, "###PROTOTYPE###", protoProv, -1)
+	_, err = n.Conn1.ExecNeo(tmpExportQuery, nil)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Print first cypher again, changed OPTIONAL part to be fixed to 'pre'.
 
 	fmt.Printf(" done\n\n")
 
